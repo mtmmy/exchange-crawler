1f7abfa3e9dba65c7f530db1a690bc51cf6ee775
mtmmy <mtmmy@users.noreply.github.com>
2017-09-04 08:33:57 +0800

    Structure

---
+++ src/component/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { requestStock } from '../actions'
+
+class App extends React.Component {
+
+
+}
